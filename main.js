2d48c6089c8688ad6f552d14c7a9747613cbd1b2
Brushteyn Timofey <brushteyntimofey@gmail.com>
2021-12-13 14:59:27 +0500

    error add when input empty

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
         addTodoInput: '',
-        lists: []
+        lists: [],
+        hasError: false
     },
     methods: {
         addTodo() {
+            if (!this.addTodoInput) {
+                this.hasError = true;
+                return;
+            }
+
+            this.hasError = false;
+
             this.lists.push({
                 id: this.lists.length + 1,
                 title: this.addTodoInput,
